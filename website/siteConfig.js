a7cae2e050ebe016fa7b25052cb8dfbe207ed99f
woliveiras <w.oliveira542@gmail.com>
2020-02-26 07:15:46 -0300

    Add projectName

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,14 +23,11 @@ const users = [
 const siteConfig = {
   title: 'Capacita.dev', // Title for your website.
   tagline: 'Aprenda programação online de graça através de desafios práticos',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  url: 'https://www.capacita.dev/', // Your website URL
   baseUrl: '/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
+  projectName: 'Capacita.dev',
   organizationName: 'facebook',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
