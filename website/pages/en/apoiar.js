99dd629f9bb3116ce5bd2552883b5a24c8018794
woliveiras <w.oliveira542@gmail.com>
2020-02-26 07:21:16 -0300

    Update configs

--- website/pages/en/apoiar.js
+++ website/pages/en/apoiar.js
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 const React = require('react')
 
 const CompLibrary = require('../../core/CompLibrary.js')
@@ -13,12 +6,6 @@ const Container = CompLibrary.Container
 const GridBlock = CompLibrary.GridBlock
 
 function Help (props) {
-  const { config: siteConfig, language = '' } = props
-  const { baseUrl, docsUrl } = siteConfig
-  const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`
-  const langPart = `${language ? `${language}/` : ''}`
-  const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`
-
   const supportLinks = [
     {
       content: 'Para apoiar acesse: [PicPay - William Oliveira](https://picpay.me/etc.william)',
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
 
@@ -28,7 +21,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'Capacita.dev',
-  organizationName: 'facebook',
+  organizationName: 'woliveiras',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -41,9 +34,7 @@ const siteConfig = {
   //   { blog: true, label: 'Blog' }
   // ],
 
-  headerLinks: [
-    { page: 'apoiar', label: 'Apoiar' }
-  ],
+  headerLinks: [],
 
   // If you have users set above, you add it here:
   users,
