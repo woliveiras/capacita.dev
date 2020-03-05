2208928fadcfc7aec2c25d388fa3abd0e3c23882
woliveiras <w.oliveira542@gmail.com>
2020-03-05 09:10:15 -0300

    Add GlobalStyles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
+import { createGlobalStyle } from 'styled-components'
 
 import Header from './header'
 import { Container, Content } from './containers'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family:-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
+  }
+`
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -18,6 +25,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <GlobalStyle theme='purple' />
       <Header siteTitle={data.site.siteMetadata.title} />
 
       <Container>
