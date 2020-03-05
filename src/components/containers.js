1a676af6e3a18dbe12d294487d711d6c0c19cfd3
woliveiras <w.oliveira542@gmail.com>
2020-03-05 09:16:32 -0300

    Update Container for media queries

--- src/components/containers.js
+++ src/components/containers.js
@@ -7,6 +7,12 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  flex-direction: column;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
 `
 
 export const Content = styled.div`
